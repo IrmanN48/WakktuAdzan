117e8c4061197d447eba7b6c872fd39fcd1b4528
irman <irmanthea12@gmail.com>
2021-07-31 19:32:00 +0700

    initial commit

---
+++ app/src/main/java/com/azan/types/BaseTimeAdjustmentType.java
@@ -0,0 +1,8 @@
+package com.azan.types;
+
+
+
+public enum BaseTimeAdjustmentType {
+    TWO_MINUTES_ADJUSTMENT,
+    TWO_MINUTES_ZUHR
+}
---
+++ app/src/main/java/com/azan/types/PrayersType.java
@@ -0,0 +1,21 @@
+package com.azan.types;
+
+
+
+public enum PrayersType {
+    FAJR(0),
+    SUNRISE(1),
+    ZUHR(2),
+    ASR(3),
+    MAGHRIB(4),
+    ISHA(5);
+    private int index;
+
+    public int getIndex() {
+        return index;
+    }
+
+    PrayersType(int index) {
+        this.index = index;
+    }
+}
